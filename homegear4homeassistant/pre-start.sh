5af015ee6d7c3b49dc46c89211391616ce40a5cb
Oliver Schneider <schneider@teamemo.com>
2022-10-02 00:35:45 +0200

    Link fixes

--- homegear4homeassistant/pre-start.sh
+++ homegear4homeassistant/pre-start.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 # map config and share to homegear volume mount points
-ln -sv /config /etc/homegear
+ln -svf -T /config /etc/homegear
 mkdir -p /share/homegear/lib
-ln -sv /share/homegear/lib /var/lib/homegear
+ln -svf -T /share/homegear/lib /var/lib/homegear
 mkdir -p /share/homegear/log
-ln -sv /share/homegear/log /var/log/homegear
+ln -svf -T /share/homegear/log /var/log/homegear
 /start.sh
